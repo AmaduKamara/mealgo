fc5cc8e67d6aedddcf67396515a39b901e57be3d
Amkam <amaduskamara@gmail.com>
2023-01-02 11:52:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
