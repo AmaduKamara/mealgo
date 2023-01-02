01ba966030c3b04bf5a98e410ad7e133009343a5
Amkam <amaduskamara@gmail.com>
2023-01-02 12:11:32 +0000

    Update Card.jsx

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./Card.module.css";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ children }) => {
+  return <div className={styles.card}>{children}</div>;
 };
 
 export default Card;
