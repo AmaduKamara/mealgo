c0011769d99b873509fd964ed9927a8428006f3f
Amkam <amaduskamara@gmail.com>
2023-01-02 11:39:47 +0000

    Used HeaderCartButton in Header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import styles from "./Header.module.css";
 
 import meals from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className={styles.header}>
         <h1>MealGo</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={meals} alt="Table with delicious Meals" />
