765e57459d74a5df03fbc65de92f3f9e448e9586
Amkam <amaduskamara@gmail.com>
2023-01-02 12:11:17 +0000

    Update AvailableMeals.jsx

--- src/components/meals/AvailableMeals.jsx
+++ src/components/meals/AvailableMeals.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Card from "../ui/Card";
 
 import styles from "./AvailableMeals.module.css";
+import MealItem from "./meal-item/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -30,11 +32,15 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem key={meal.id} meal={meal} />
+  ));
 
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
